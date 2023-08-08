8ce00dd8f466e56f1c5bece93638b140eb2ea26e
DenerBorges <denerkborges@bol.com.br>
2023-08-08 15:42:53 -0300

    feat: adding project route

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Categories from "../pages/Categories";
 import Home from "../pages/Home";
+import Project from "../pages/Project";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 
@@ -14,6 +15,7 @@ const RoutesApp: React.FC = () => {
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/project" element={<Project />} />
           <Route path="/categories" element={<Categories />} />
         </Routes>
       </Fragment>
