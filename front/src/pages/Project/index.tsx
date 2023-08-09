da6b30f60dc3620abe93ff8109ec8748b252cdea
DenerBorges <denerkborges@bol.com.br>
2023-08-09 20:14:32 -0300

    feat: adding carousel and navigation

--- front/src/pages/Project/index.tsx
+++ front/src/pages/Project/index.tsx
@@ -1,6 +1,9 @@
 import {
+  Bars3BottomLeftIcon,
+  ChatBubbleBottomCenterTextIcon,
   CurrencyDollarIcon,
   MagnifyingGlassIcon,
+  ShareIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/solid";
 import React from "react";
@@ -68,25 +71,95 @@ const Project: React.FC = () => {
       <h1 className="text-center shadow fw-bolder py-2 my-3">
         Nome do Projeto
       </h1>
-      <div className="container text-center bg-light bg-gradient my-5 mx-auto">
+      <div className="container shadow border rounded text-center bg-light bg-gradient my-5 mx-auto">
         <div className="row row-cols-lg-2 row-cols-1 border secondary">
           <div className="col">
-            <img
-              src={require("../../assets/images/default_product.png")}
-              className="img-fluid"
-              alt="imagem do projeto"
-            />
+            <div
+              id="carouselExampleIndicators"
+              className="carousel carousel-dark slide border-end border-3"
+            >
+              <div className="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="0"
+                  className="active"
+                  aria-current="true"
+                  aria-label="Slide 1"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide-to="2"
+                  aria-label="Slide 3"
+                ></button>
+              </div>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src={require("../../assets/images/categorie_art.png")}
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src={require("../../assets/images/categorie_book.png")}
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src={require("../../assets/images/categorie_game.png")}
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                </div>
+              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
           </div>
+
           <div className="col p-4">
             <u className="text-start fw-medium">Criador: Nome do criador</u>
-            <div className="m-5">
+            <div className="m-5 py-3">
               <p className="text-start fw-semibold">
                 Atingido: <span className="text-info">50%</span>
               </p>
               <div
                 className="progress"
                 role="progressbar"
-                aria-label="Basic example"
+                aria-label="Progress bar"
                 aria-valuenow={50}
                 aria-valuemin={0}
                 aria-valuemax={100}
@@ -98,7 +171,7 @@ const Project: React.FC = () => {
               </p>
             </div>
             <div className="mx-5">
-              <h2 className="fw-bolder">
+              <h2 className="fw-bolder py-4">
                 Arrecadado: <span className="text-info">R$ 200</span>
               </h2>
             </div>
@@ -110,6 +183,62 @@ const Project: React.FC = () => {
               <CurrencyDollarIcon className="dollarIcon" />
             </button>
           </div>
+        </div>
+      </div>
+
+      <div className="container shadow p-0 mx-auto">
+        <nav className="nav nav-pills nav-fill rounded rounded-bottom-0 fw-medium">
+          <a
+            className="nav-link border bg-gradient border-dark rounded-end-0 rounded-bottom-0 rounded-top-1 text-light active"
+            aria-current="page"
+            href="/project"
+          >
+            Sobre o projetos
+            <Bars3BottomLeftIcon className="navIcons" />
+          </a>
+          <a
+            className="nav-link bg-secondary bg-gradient border border-dark rounded-0 text-light"
+            href="/project"
+          >
+            Comentários
+            <ChatBubbleBottomCenterTextIcon className="navIcons" />
+          </a>
+          <a
+            className="nav-link bg-secondary bg-gradient border border-dark rounded-start-0 rounded-bottom-0 rounded-top-1 text-light"
+            href="/project"
+          >
+            Compartilhar
+            <ShareIcon className="navIcons" />
+          </a>
+        </nav>
+        <div className="col bg-light border border-top-0 border-secondary rounded-bottom-1 p-4">
+          <h2 className="text-center pb-4 fw-bolder">Nome do projeto</h2>
+          <p className="text-center fw-medium px-5">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque
+            eveniet pariatur ratione. Dolorem obcaecati neque ipsa fuga veniam
+            delectus earum ullam, esse magnam nihil assumenda mollitia cum
+            placeat, unde necessitatibus impedit est repellendus hic odio
+            laboriosam, consequatur alias! Esse quidem officiis nesciunt saepe
+            voluptatibus voluptates amet molestiae, nisi, soluta pariatur, sit
+            delectus expedita cupiditate mollitia quo inventore consequuntur
+            deserunt excepturi possimus quaerat deleniti autem illum ullam
+            sapiente! Nemo blanditiis iure corporis adipisci! Minus commodi ipsa
+            consequatur voluptas tempore rerum nam reprehenderit odit dolorum
+            aperiam sint amet consectetur iure esse quasi, excepturi dolores
+            architecto omnis dolor soluta. Veniam error quaerat voluptatum
+            voluptatibus obcaecati excepturi repudiandae tempora quidem, eum
+            voluptas voluptate fugit, debitis delectus quo nobis saepe. Quidem,
+            doloribus consequuntur vel consequatur id magnam accusantium ad
+            ipsa, natus delectus distinctio dolor nemo. Sed nam corrupti, totam
+            ipsa aliquid vero natus. Sit perferendis veritatis voluptates
+            quibusdam ea labore excepturi, dolor, eius ad odit inventore libero!
+            Ratione tenetur rem iste nihil. Aspernatur eaque dolorum
+            reprehenderit temporibus itaque est animi iure et, deleniti possimus
+            fuga quaerat vitae atque a in ducimus iusto harum debitis
+            perferendis laborum officiis cumque dicta qui. Sapiente quae totam
+            illo dolor tempora aliquam, dolore fugit quis ratione, quaerat
+            nesciunt esse corrupti?
+          </p>
         </div>
       </div>
 
