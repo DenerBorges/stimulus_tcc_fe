228ea3a2ea3c890043b91e1b6e4255f076d1bbc1
DenerBorges <denerkborges@bol.com.br>
2023-08-12 16:11:19 -0300

    refactor: changing port and adding global prefix

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
+  app.setGlobalPrefix('/api/v1/');
+
+  await app.listen(5000);
 }
 bootstrap();
