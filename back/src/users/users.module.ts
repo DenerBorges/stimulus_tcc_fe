72c0f2d3ea22b957f74397f1e18b729f6248678c
DenerBorges <denerkborges@bol.com.br>
2023-08-17 19:10:31 -0300

    feat: exporting user service

--- back/src/users/users.module.ts
+++ back/src/users/users.module.ts
@@ -10,5 +10,6 @@ import { User, UserSchema } from './entities/user.entity';
   ],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
