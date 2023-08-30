584059d68baf949fd3a98b89ed20424c1a1fcca7
DenerBorges <denerkborges@bol.com.br>
2023-08-30 16:07:55 -0300

    feat: creating routes for two new pages

--- front/src/routes/index.tsx
+++ front/src/routes/index.tsx
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Categories from "../pages/Categories";
 import Home from "../pages/Home";
+import Categories from "../pages/Categories";
 import Project from "../pages/Project";
+import CreateProject from "../pages/CreateProject";
+import Reward from "../pages/Reward";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 
@@ -15,8 +17,10 @@ const RoutesApp: React.FC = () => {
           <Route path="/" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/project" element={<Project />} />
           <Route path="/categories" element={<Categories />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/create_project" element={<CreateProject />} />
+          <Route path="/reward" element={<Reward />} />
         </Routes>
       </Fragment>
     </BrowserRouter>
