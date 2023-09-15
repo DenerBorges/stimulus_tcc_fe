32e7180f6af3eb93eec9dc4bfb311f04c1044949
DenerBorges <denerkborges@bol.com.br>
2023-09-15 14:55:37 -0300

    feat: changing env file references

--- front/src/utils/api.ts
+++ front/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
