78fd936c2e33cea1ab41e60f189a18b67fb77261
DenerBorges <denerkborges@bol.com.br>
2023-09-23 11:52:04 -0300

    feat: setting base url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  // baseURL: "https://stimulus.fly.dev/api/v1/",
+  baseURL: "http://localhost:5000/api/v1/",
 });
 
 api.interceptors.request.use((config) => {
