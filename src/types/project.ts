49e270a7b2bf9f503b15c2d639da9ed8353b5fb0
DenerBorges <denerkborges@bol.com.br>
2023-10-01 11:48:09 -0300

    feat: creating user and project types

---
+++ src/types/project.ts
@@ -0,0 +1,10 @@
+export interface projectType {
+  id: number;
+  name: string;
+  description: string;
+  category: string;
+  goal: number;
+  deadline: number;
+  image: string[];
+  userId: number;
+}
