3e096d0502aa6a15b0d90d7d8248a9badd0bc0f0
DenerBorges <denerkborges@bol.com.br>
2024-07-17 16:04:14 -0300

    refactor: sorting projects by id and reports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,6 +28,9 @@ const Home: React.FC = () => {
     try {
       const response = await api.get("projects");
       const allProjects = response.data;
+
+      allProjects.sort((a: projectType, b: projectType) => a.id - b.id);
+
       setProjects(allProjects);
 
       const highlighted = allProjects.filter(
@@ -45,6 +48,13 @@ const Home: React.FC = () => {
         },
         {}
       );
+
+      Object.keys(categorized).forEach((category) => {
+        categorized[category].sort(
+          (a: projectType, b: projectType) => a.id - b.id
+        );
+      });
+
       setCategories(categorized);
 
       const initialVisibleProjects = Object.keys(categorized).reduce(
@@ -106,100 +116,100 @@ const Home: React.FC = () => {
     visibleCount: number,
     category?: string
   ) => {
-    return projects.length === 0
-      ? [1, 2, 3, 4, 5, 6, 7, 8].map((n) => <Loading key={n} />)
-      : projects
-          .slice(0, visibleCount)
-          .sort((a, b) => a.deadline - b.deadline)
-          .map((project) => {
-            const progress = Math.min(
-              (project.total / project.goal) * 100,
-              100
-            );
-            const progressBarColor =
-              progress === 100 ? "bg-success" : "bg-info";
-            const borderClass =
-              moderator && project.report > 0 ? "border-danger border-4" : "";
-            return (
-              <div className="col" key={project.id}>
-                <div className={`card shadow-sm h-100 ${borderClass}`}>
-                  <a href={`/project/${project.id}`}>
-                    {!isBase64Image(project.image[0]) ? (
-                      <img
-                        src={
-                          project.image.length > 0
-                            ? project.image[0]
-                            : defaultImage
-                        }
-                        className="card-img-top border secondary"
-                        alt={project.name}
-                      />
-                    ) : (
-                      <img
-                        src={`data:image/jpeg;base64,${project.image[0]}`}
-                        className="card-img-top border secondary"
-                        alt={project.name}
-                      />
-                    )}
-                  </a>
-                  <div className="d-flex flex-column card-body">
-                    <div className="mb-auto pb-2">
-                      <h5 className="card-title text-center">{project.name}</h5>
-                      <a
-                        className="category-link card-text"
-                        href={(() => {
-                          let categoryPath = "/";
-                          if (project.category === "Arte") {
-                            categoryPath = "/art";
-                          } else if (project.category === "Gastronomia") {
-                            categoryPath = "/gastronomy";
-                          } else if (project.category === "Jogo") {
-                            categoryPath = "/game";
-                          } else if (project.category === "Livro") {
-                            categoryPath = "/book";
-                          } else if (project.category === "Música") {
-                            categoryPath = "/music";
-                          } else if (project.category === "Social") {
-                            categoryPath = "/social";
-                          } else if (project.category === "Tecnologia") {
-                            categoryPath = "/technology";
-                          } else if (project.category === "Outros") {
-                            categoryPath = "/others";
-                          }
+    let sortedProjects = [...projects];
+    if (moderator) {
+      sortedProjects.sort((a, b) => b.report - a.report || a.id - b.id);
+    } else {
+      sortedProjects.sort((a, b) => a.id - b.id);
+    }
 
-                          return categoryPath;
-                        })()}
-                      >
-                        <small>{project.category}</small>
-                      </a>
-                      <p className="card-text">{project.description}</p>
-                    </div>
-                    <div
-                      className="progress"
-                      role="progressbar"
-                      aria-label="Basic example"
-                      aria-valuenow={progress}
-                      aria-valuemin={0}
-                      aria-valuemax={100}
+    return sortedProjects.length === 0
+      ? [1, 2, 3, 4, 5, 6, 7, 8].map((n) => <Loading key={n} />)
+      : sortedProjects.slice(0, visibleCount).map((project) => {
+          const progress = Math.min((project.total / project.goal) * 100, 100);
+          const progressBarColor = progress === 100 ? "bg-success" : "bg-info";
+          const borderClass =
+            moderator && project.report > 0 ? "border-danger border-4" : "";
+          return (
+            <div className="col" key={project.id}>
+              <div className={`card shadow-sm h-100 ${borderClass}`}>
+                <a href={`/project/${project.id}`}>
+                  {!isBase64Image(project.image[0]) ? (
+                    <img
+                      src={
+                        project.image.length > 0
+                          ? project.image[0]
+                          : defaultImage
+                      }
+                      className="card-img-top border secondary"
+                      alt={project.name}
+                    />
+                  ) : (
+                    <img
+                      src={`data:image/jpeg;base64,${project.image[0]}`}
+                      className="card-img-top border secondary"
+                      alt={project.name}
+                    />
+                  )}
+                </a>
+                <div className="d-flex flex-column card-body">
+                  <div className="mb-auto pb-2">
+                    <h5 className="card-title text-center">{project.name}</h5>
+                    <a
+                      className="category-link card-text"
+                      href={(() => {
+                        let categoryPath = "/";
+                        if (project.category === "Arte") {
+                          categoryPath = "/art";
+                        } else if (project.category === "Gastronomia") {
+                          categoryPath = "/gastronomy";
+                        } else if (project.category === "Jogo") {
+                          categoryPath = "/game";
+                        } else if (project.category === "Livro") {
+                          categoryPath = "/book";
+                        } else if (project.category === "Música") {
+                          categoryPath = "/music";
+                        } else if (project.category === "Social") {
+                          categoryPath = "/social";
+                        } else if (project.category === "Tecnologia") {
+                          categoryPath = "/technology";
+                        } else if (project.category === "Outros") {
+                          categoryPath = "/others";
+                        }
+
+                        return categoryPath;
+                      })()}
                     >
-                      <div
-                        className={`progress-bar ${progressBarColor}`}
-                        style={{ width: `${progress}%` }}
-                      ></div>
+                      <small>{project.category}</small>
+                    </a>
+                    <p className="card-text">{project.description}</p>
+                  </div>
+                  <div
+                    className="progress"
+                    role="progressbar"
+                    aria-label="Basic example"
+                    aria-valuenow={progress}
+                    aria-valuemin={0}
+                    aria-valuemax={100}
+                  >
+                    <div
+                      className={`progress-bar ${progressBarColor}`}
+                      style={{ width: `${progress}%` }}
+                    ></div>
+                  </div>
+                  <div className="row pt-2">
+                    <div className="col pe-0">
+                      <small>{`${progress.toFixed(0)} %`}</small>
                     </div>
-                    <div className="row pt-2">
-                      <div className="col pe-0">
-                        <small>{`${progress.toFixed(0)} %`}</small>
-                      </div>
-                      <div className="col p-0">
-                        <small>{project.deadline} dias restantes</small>
-                      </div>
+                    <div className="col p-0">
+                      <small>{project.deadline} dias restantes</small>
                     </div>
                   </div>
                 </div>
               </div>
-            );
-          });
+            </div>
+          );
+        });
   };
 
   return (
